badcd2e3e55eedaaaecd06379b2eed614e7c6399
Gabriel <gabrielmesmo97@gmail.com>
2024-05-18 16:11:35 -0300

    Question 5 done

--- q5/icestorm.lua
+++ q5/icestorm.lua
@@ -1,42 +1,75 @@
 -- NOTE(Gabriel): Searched up and there is a constant called CONST_ME_ICETORNADO
 -- NOTE(Gabriel): https://github.com/otland/forgottenserver/wiki/Constants#MagicEffect
-local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, COMBAT_ICETORNADO)
+local combat = {}
 
-local area = createCombatArea(AREA_CIRCLE5X5)
-setCombatArea(combat, area)
+local areas = {
+   {
+      {0, 0, 0, 0, 0},
+      {0, 1, 0, 0, 0},
+      {1, 0, 2, 0, 1},
+      {0, 1, 0, 0, 0},
+      {0, 0, 1, 0, 0},
+   },
+   {
+      {1, 0, 1},
+      {0, 2, 0},
+      {1, 0, 1},
+   },
+   {
+      {0, 0, 1},
+      {1, 2, 0},
+      {0, 0, 1},
+   },
+   {
+      {0, 0, 0, 0, 0},
+      {0, 1, 0, 1, 0},
+      {0, 0, 2, 0, 1},
+      {0, 0, 1, 1, 0},
+      {0, 0, 1, 0, 0},
+   },
+   {
+      {0, 0, 1},
+      {1, 2, 0},
+      {0, 0, 1},
+   },
+   {
+      {1, 1, 0},
+      {1, 2, 0},
+      {1, 0, 1},
+   },
+}
 
-function onGetFormulaValues(cid, level, magicLevel)
-   local levelDiv, magicMult, bias = 5, 3.14, 10
-   local min = (level / levelDiv) + (magicLevel * magicMult) + bias
-   local max = (level / levelDiv) + (magicLevel * magicMult * 2) + bias * 2
+-- NOTE(Gabriel): We need to initialize a new combat for each pattern :/
+for i = 1, #areas do
+   combat[i] = Combat()
+   combat[i]:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+   combat[i]:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
 
-   return min, max
+   combat[i]:setArea(createCombatArea(areas[i]))
 end
-
-setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 local iceStorm = Spell(SPELL_INSTANT)
 function iceStorm.onCastSpell(creature, variant)
-   -- NOTE(Gabriel): Now, let's do the cool effect stuff!
-   -- NOTE(Gabriel): Let's start by just spawning a single effect
-   creature:getPosition():sendMagicEffect(CONST_ME_ICETORNADO)
+   local cooldown = 250
+   
+   for i = 1, #areas do
+      addEvent(function()
+	    -- NOTE(Gabriel): Iterate over combats
+	    combat[i]:execute(creature, variant)
+      end, (cooldown * i) - cooldown)
+   end
 
-   -- NOTE(Gabriel): Okay, now that is working I can make the cools effect position and timing
-   
-   
-   return combat:execute(creature, variant)
+   return true
 end
 
 iceStorm:name("Ice Storm")
-iceStorm:id(6969)
+iceStorm:id(69)
 iceStorm:words("frigo")
 iceStorm:level(1)
 iceStorm:mana(0)
 iceStorm:group("attack")
-iceStorm:cooldown(2000)
-iceStorm:groupCooldown(2000)
+iceStorm:cooldown(50)
+iceStorm:groupCooldown(50)
 iceStorm:isSelfTarget(false)
 iceStorm:isAggressive(true)
 iceStorm:needLearn(false)
